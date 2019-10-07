a5947e8dfd0610277100c9c5a89de77caf5c4023
rishav <rishavgarg789@gmail.com>
2019-10-07 04:49:44 -0400

    commit

--- app/src/main/java/com/example/secure/MainActivity.java
+++ app/src/main/java/com/example/secure/MainActivity.java
@@ -1,14 +1,73 @@
 package com.example.secure;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editTextEmail, editTextPassword;
+    Button buttonSignin;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editTextEmail = (EditText) findViewById(R.id.email);
+        editTextPassword = (EditText) findViewById(R.id.password);
+        buttonSignin = (Button) findViewById(R.id.signin);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        buttonSignin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SigninUser();
+            }
+        });
+
+    }
+
+    private void SigninUser() {
+        String email = editTextEmail.getText().toString();
+        String password = editTextPassword.getText().toString();
+
+        if (email.isEmpty()) {
+            Toast.makeText(MainActivity.this, "Enter your Email", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (password.isEmpty()) {
+            Toast.makeText(MainActivity.this, "Enter your Password", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(MainActivity.this, "Try Again", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+
+                        Intent intent = new Intent(MainActivity.this, Signup.class);
+                        startActivity(intent);
+
+                    }
+                });
     }
 }
